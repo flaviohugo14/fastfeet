9058ec53d01ae72dffef477e86015028c966e946
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-01-31 19:29:39 -0300

    .env update

--- server/src/config/auth.js
+++ server/src/config/auth.js
@@ -1,4 +1,8 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
-  secret: 'b3bde8ee000968ca6c67464d3abc5560',
+  secret: process.env.JWT_SECRET,
   expiresIn: '7d',
 };
--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'fastfeet',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
