70f0cb55b4e0b994a932bb85c326019f92359ca0
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-04-17 14:25:37 -0300

    feature: Show Action Buttons

--- mobile/src/pages/DeliveryDetails/Details/DeliveryActions/index.js
+++ mobile/src/pages/DeliveryDetails/Details/DeliveryActions/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import IconCommunity from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {
+  Container,
+  Text,
+  InfoProblem,
+  ShowProblems,
+  ConfirmDelivery,
+} from './styles';
 
 export default function DeliveryActions() {
-  return <View />;
+  return (
+    <Container>
+      <InfoProblem>
+        <Icon color="#e74040" name="highlight-off" size={28} />
+        <Text>Informar Problema</Text>
+      </InfoProblem>
+      <ShowProblems>
+        <Icon color="#E7BA40" name="info-outline" size={28} />
+        <Text>Visualizar Problemas</Text>
+      </ShowProblems>
+      <ConfirmDelivery>
+        <IconCommunity color="#7D40E7" name="check-circle-outline" size={28} />
+        <Text>Confirmar Entrega</Text>
+      </ConfirmDelivery>
+    </Container>
+  );
 }
--- mobile/src/pages/DeliveryDetails/Details/DeliveryActions/styles.js
+++ mobile/src/pages/DeliveryDetails/Details/DeliveryActions/styles.js
@@ -1,3 +1,53 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import { Dimensions } from 'react-native';
+
+export const Container = styled.View`
+  margin: 13px 22px 0;
+  flex-direction: row;
+  border-radius: 4px;
+  background: #f8f9fd;
+  height: 100px;
+
+  box-shadow: 0 0 3px #0000001a;
+  elevation: 3;
+`;
+
+export const Text = styled.Text`
+  color: #999;
+  font-size: 14px;
+  overflow: hidden;
+  max-width: 80px;
+  text-align: center;
+
+  margin-top: 2px;
+`;
+
+export const InfoProblem = styled.TouchableOpacity`
+  width: ${(Dimensions.get('window').width - 44) / 3}px;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+
+  border-style: solid;
+  border-right-color: #0000001a;
+  border-right-width: 1px;
+`;
+
+export const ShowProblems = styled.TouchableOpacity`
+  width: ${(Dimensions.get('window').width - 44) / 3}px;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ConfirmDelivery = styled.TouchableOpacity`
+  width: ${(Dimensions.get('window').width - 44) / 3}px;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+
+  border-style: solid;
+  border-left-color: #0000001a;
+  border-left-width: 1px;
+`;
